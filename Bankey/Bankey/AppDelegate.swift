8f406259ed523c4c3ee53f0e1fb1fd7eab075bbc
Nikos Galinos <ngalinos95@me.com>
2024-04-10 23:13:38 +0300

    remove storyboards

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Nikos Galinos on 10/4/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let mainPage = ViewController()
+        let nav = UINavigationController(rootViewController: mainPage)
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+        window?.rootViewController = nav
+        return true
+    }
+}
+
---
+++ Bankey/Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Nikos Galinos on 10/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
